0ed95c0b7e3e6e523a763107befe0a4172de4311
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-04-17 17:03:19 -0300

    Teste finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+enum TransactionType {
+  income = 'income',
+  outcome = 'outcome',
 }
 
 class TransactionsRepository {
@@ -13,16 +24,37 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
+  private calculateBalance(operator: TransactionType): number {
+    return this.transactions
+      .filter(t => t.type === operator)
+      .reduce((accumulated, transaction) => accumulated + transaction.value, 0);
+  }
+
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.calculateBalance(TransactionType.income);
+    const outcome = this.calculateBalance(TransactionType.outcome);
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const { total } = this.getBalance();
+    if (type === TransactionType.outcome && total < value) {
+      throw Error('Cannot create outcome transaction without a valid balance');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
